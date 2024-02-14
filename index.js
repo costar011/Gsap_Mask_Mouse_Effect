43f5bc8cf78b8466722db0a122e45aa9a5b10b77
costar011 <dnaer456@gmail.com>
2024-02-14 17:15:03 +0900

    js

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ let xTo = gsap.quickTo(".hideen-content", "--x", {
     duration: 0.4,
     ease: "power4.out",
   });
+
+let t1 = gsap.timeline({ paused: true });
+t1.to(".hideen-content", {
+  "--size": 250,
+  duration: 0.75,
+  ease: "back.out(1.7)",
+});
+
+let hoveringContent = gsap.utils.toArray("p", content);
