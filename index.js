fbcc99e8835c0ede85359f0c0c4a1a690f4b4348
costar011 <dnaer456@gmail.com>
2024-02-19 23:41:47 +0900

    js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const content = document.querySelector(".content");
 
-let xTo = gsap.quickTo(".hidden-content", "--x",  {
+let xTo = gsap.quickTo(".hidden-content", "--x", {
     duration: 0.4,
-    ease: "power4.out"
-});
+    ease: "power4.out",
+  }),
+  yTo = gsap.quickTo(".hidden-content", "--y", {
+    duration: 0.4,
+    ease: "power4.out",
+  });
