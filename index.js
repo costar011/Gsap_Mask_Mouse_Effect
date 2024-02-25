4bc82eaab6fb267cea67965021d063590514b9e1
costar011 <dnaer456@gmail.com>
2024-02-25 23:04:37 +0900

    js

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ let xTo = gsap.quickTo(".hidden-content", "--x", {
     duration: 0.4,
     ease: "power4.out",
   });
+
+let t1 = gsap.timeline({ paused: true });
+t1.to(".hidden-content", {
+  "--size": 250,
+  duration: 0.75,
+  ease: "back.out(1.7)",
+});
+
+/*let hoveringContent.forEach((e1) => {
+    e1.addEventListener("mouseenter", () => {
+      t1.restart();
+    })
+  });
+  */
